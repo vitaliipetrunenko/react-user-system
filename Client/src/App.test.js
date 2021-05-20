910a66ee07a5f24a6984006a5df56898e8af841a
Vitalii Petrunenko <vitaliipetrunenko@gmail.com>
2021-05-20 12:07:12 +0300

    test reworks

--- Client/src/App.test.js
+++ Client/src/App.test.js
@@ -1,34 +1,134 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import * as Calls from "./apiCalls/apiCalls";
+
 import App from "./App";
 import { Provider } from "react-redux";
 import authReducer from "./redux/authReducer";
 
+import profileData from "./redux/profilesReducer";
+import authData from "./redux/authReducer";
+
+import useToken from "./hooks/useToken";
+import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-dom/test-utils";
+import { LocalStorageMock as LocStor }  from  '@react-mock/localstorage';
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {token:defaultToken};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+};
+
+let defaultToken = {
+  token:"testtoken",
+  expiry:'1000000000000000000000000000000'
+};
+
+
+global.localStorage = new LocalStorageMock;
+
+
+const rootReducer = combineReducers({ profileData, authData });
 const renderWithRedux = (
   component,
-  { defaultUsers, store = createStore(authReducer, defaultUsers) } = {}
+  {
+    defaultUsers,
+    store = createStore(rootReducer, {
+      profileList: [
+        { _id: 1, name: "test", author: "mail" },
+        { _id: 2, name: "test2", author: "mail" },
+      ],
+      currUser: { email: "mail", role: "ADMIN" },
+    }),
+  } = {}
 ) => {
   return {
     ...render(<Provider store={store}>{component}</Provider>),
   };
 };
 
-it("Renders app", async () => {
+
+
+it("Renders app with valid token", async () => {
+  const { result } = renderHook(() => useToken())
   Calls.loadUserFromToken = jest.fn().mockImplementation(() =>
     Promise.resolve({
       email: "loadedfromtoken",
+      token: "token"
     })
   );
+  await waitFor(()=>{
+    result.current.setToken(defaultToken)
+  })
   const app = renderWithRedux(
     <Router>
       <App />
     </Router>,
     { initialState: {} }
   );
+  
   expect(app.container).toBeInTheDocument();
+
+
 });
 
+
+it("Renders login with invalid token", async () => {
+  
+  Calls.loadUserFromToken = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      email: "loadedfromtoken",
+    })
+  );
+ 
+  const app = renderWithRedux(
+    <Router>
+      
+      <App />
+      
+    </Router>,
+    { initialState: {} }
+  );
+  
+  expect(app.container).toBeInTheDocument();
+
+
+});
+
+
+it("Renders app with valid but outdated token", async () => {
+  const { result } = renderHook(() => useToken())
+  await waitFor(()=>{
+    result.current.setToken({...defaultToken,expiry:1})
+  })
+  const app = renderWithRedux(
+    <Router>
+      <App />
+    </Router>,
+    { initialState: {} }
+  );
+  
+  expect(app.container).toBeInTheDocument();
+
+
+});
--- Client/src/components/controlpanel/UserBox.test.js
+++ Client/src/components/controlpanel/UserBox.test.js
@@ -12,11 +12,11 @@ import authData from './../../redux/authReducer';
 //import ProfileBoxHOC from './ProfileBox';
 import UserBoxHOC from './UserBox';
 
-
+let userObj={authData:{currUser:{email:"mail",role:"ADMIN"}}};
 
 //const rootReducer = combineReducers({profileData,authData})
 const renderWithRedux = (component,{
-  defaultUsers,store = createStore(authData,{authData:{currUser:{email:"mail",role:"ADMIN"}}})
+  defaultUsers,store = createStore(authData,userObj)
    } = {}) => {
 
     
@@ -30,7 +30,7 @@ const renderWithRedux = (component,{
 
 
 
-it('Renders userbox and shows user',async () => {
+it('Renders userbox and shows user, also edits it',async () => {
     
   Calls.getProfiles = jest.fn().mockImplementation(() => Promise.resolve(
     [{
@@ -71,4 +71,59 @@ it('Renders userbox and shows user',async () => {
     
   })
 
- 
+  it('Renders userbox and shows user, also fails to edit it',async () => {
+    
+    Calls.getProfiles = jest.fn().mockImplementation(() => Promise.resolve(
+      [{
+        name:"user1",
+        _id:1,
+        author:"mail"
+      },{
+          name:"user2",
+          _id:2,
+          author:"mail"
+        }]
+    ))
+   
+     Calls.fetchUsers = jest.fn().mockImplementation(() => Promise.resolve(
+     true
+    ))
+  
+     
+     const profileBox  = renderWithRedux(
+     
+     <Router>
+      <UserBoxHOC user={{name:"testname", email:"testmail", role:"testrole"}}/>
+      </Router>
+    , { initialState: {} })
+      await waitFor(() => {
+          expect(profileBox.container).toBeInTheDocument()
+          expect(profileBox.getByText("Name: testname")).toBeInTheDocument() 
+        });
+        userEvent.click(profileBox.getAllByText("Edit")[0])
+        await waitFor(() => {
+        expect(profileBox.getByText("Cancel")).toBeInTheDocument() 
+        
+      });
+      userEvent.click(profileBox.getAllByText("Cancel")[0])
+      userEvent.click(profileBox.getAllByText("Edit")[0])
+      userEvent.type(profileBox.getAllByRole('textbox',{name:"name"})[0],"edit")
+       userEvent.click(profileBox.getAllByText("Okay")[0])
+      expect(profileBox.getByDisplayValue("testnameedit")).toBeInTheDocument()
+     
+      
+    })
+
+
+    it('doesnt render for non-admins',async () => {
+      userObj.authData.currUser.role="USER";     
+       const profileBox  = renderWithRedux(
+       
+       <Router>
+        <UserBoxHOC user={{name:"testname", email:"testmail", role:"testrole"}}/>
+        </Router>
+      , { initialState: {} })
+      
+         
+        
+      })
